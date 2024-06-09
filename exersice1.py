b32619c3e455d6010c2c489ea23bf53c7df6a976
Cherepahius <goodnameforemail@gmail.com>
2024-06-09 14:37:23 +0300

    Exercise.py
    Every exercise is done and commented so i can do everything in one project

---
+++ exersice1.py
@@ -0,0 +1,46 @@
+# print('Hello world\n'  'Hello world\n'  'Hello world\n'  'Hello world')
+
+
+#a = (99 ** 3)*8
+#print(a)
+
+
+# print(5 < 3) #false
+# print(3 == 3)  # true
+# print(3 == '3')  # false
+# print('3' > 3) #can't use '>" between str and int"
+# print('Hello' == 'hello') #false
+
+# computer_brand = "Corsair"
+# print(f"I have a {computer_brand} computer")
+
+# name = "Dima"
+# age = 27
+# shoe_size = 45
+# info = (f"My name is {name}.My age is {age}.My shoe size is {shoe_size}")
+# print(info)
+
+# a = 2
+# b = 5
+# if a > b:
+#    print("Hello World")
+# else:
+#    print("Hello Earth")
+
+# number = input("Enter a number:")
+# if (number % 2) == 0:
+#    print(f"Your {number} is even!".format(number))
+# else:
+#    print(f"Your {number} is odd".format(number))
+
+#user_name = input("Enter your name:")
+#if (user_name == 'Dima'):
+#    print("WOOOOOOOOOOOOOOOW")
+#else:
+#    print("Noooooooo")
+
+height = int(input("Enter your height(in cm):"))
+if (height < 145):
+    print("You are not allowed to ride")
+else:
+    print("You are welcome")
