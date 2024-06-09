4bce55b47eb7eb60b1eceed8deec593e7e88f1d4
Cherepahius <goodnameforemail@gmail.com>
2024-06-09 15:14:23 +0300

    Create daily chalange.py

---
+++ Day 1/daily chalange.py
@@ -0,0 +1,17 @@
+user_input = (input("Enter a 10-letters length string: "))
+if len(user_input) < 10:
+    print("U need to add some letters so it will be a  10-units length string")
+elif len(user_input) > 10:
+    print("U need to delete some letters so it will be a 10-units length string") 
+else:
+    print("You are great!")      
+
+print(f"First character is - {user_input[0]}") 
+print(f"Last character is - {user_input[-1]}") 
+
+string = input("enter the string: ")
+length = len(string)
+for row in range(length):
+    for col in range(row+1):
+        print(string[col], end ="")
+    print()
