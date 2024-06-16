55826fa7cbe200cb61b543eca79fbdab159f8c88
Cherepahius <goodnameforemail@gmail.com>
2024-06-16 06:29:38 +0300

    day 5 daily and mini-project

---
+++ day5/daily/daily.py
@@ -0,0 +1,17 @@
+#Exercise 1
+
+#input_sequence = input("Enter a comma-separated sequence of words: ")
+#words = [word.strip() for word in input_sequence.split(",")]
+#sorted_words = sorted(words)
+#output_sequence = ",".join(sorted_words)
+#print(output_sequence)
+
+#Exercise 2
+
+#def longest_word(sentece):
+#    words= sentece.split()
+#    longest = max(words, key = len)
+#    return longest
+#print(longest_word("Margaret's toy is a pretty doll."))  
+#print(longest_word("A thing of beauty is a joy forever."))
+#print(longest_word("Forgetfulness is by all means powerless!")) 
