23b28163187438c62ef2b46885c25c6636dc2a6f
Cherepahius <goodnameforemail@gmail.com>
2024-06-16 07:16:02 +0300

    Added exercise 2

--- Day 2/daily chalange.py
+++ Day 2/daily chalange.py
@@ -12,3 +12,18 @@
 
 #--------Exercise 2---------------
 
+def remove_extra_letters(word):
+    if not word:
+        return()
+    
+    clean_word = word[0]
+
+    for i in range(1, len(word)):
+        if word[i] !=word[i - 1]:
+            clean_word += word[i]
+    return clean_word
+user_input = input("Enter a word:")
+
+clean_word = remove_extra_letters(user_input)
+print(f"Your word: {user_input}")
+print(f"Correct word: {clean_word}")
