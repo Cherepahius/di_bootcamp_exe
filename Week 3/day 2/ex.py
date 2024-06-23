3d62d81af0a50d3759a6b7e4f27f8a5dea1da9d4
Cherepahius <goodnameforemail@gmail.com>
2024-06-23 06:34:31 +0300

    Update ex.py

--- Week 3/day 2/ex.py
+++ Week 3/day 2/ex.py
@@ -84,4 +84,109 @@
 #print(dog1.fight(dog3))
 
 
+#Ex.3
 
+
+#Ex4
+
+class Family:
+    def __init__(self, last_name, members):
+        self.last_name = last_name
+        self.members = members
+
+    def born(self, **kwargs):
+        self.members.append(kwargs)
+        print(f"Congratulations to the {self.last_name} family on the birth of {kwargs['name']}!")
+
+    def is_18(self, name):
+        for member in self.members:
+            if member['name'] == name:
+                return member['age'] >= 18
+        return False
+
+    def family_presentation(self):
+        print(f"Family Last Name: {self.last_name}")
+        for member in self.members:
+            print(f"Name: {member['name']}, Age: {member['age']}, Gender: {member['gender']}, Is Child: {member['is_child']}")
+
+
+members = [
+    {'name': 'Michael', 'age': 35, 'gender': 'Male', 'is_child': False},
+    {'name': 'Sarah', 'age': 32, 'gender': 'Female', 'is_child': False}
+]
+
+my_family = Family("Smith", members)
+
+
+my_family.born(name='James', age=0, gender='Male', is_child=True)
+print(my_family.is_18('Michael'))  # Should return True
+print(my_family.is_18('James'))    # Should return False
+my_family.family_presentation()
+
+
+#ex.5
+
+
+class Family:
+    def __init__(self, last_name, members):
+        self.last_name = last_name
+        self.members = members
+
+    def born(self, **kwargs):
+        self.members.append(kwargs)
+        print(f"Congratulations to the {self.last_name} family on the birth of {kwargs['name']}!")
+
+    def is_18(self, name):
+        for member in self.members:
+            if member['name'] == name:
+                return member['age'] >= 18
+        return False
+
+    def family_presentation(self):
+        print(f"Family Last Name: {self.last_name}")
+        for member in self.members:
+            print(f"Name: {member['name']}, Age: {member['age']}, Gender: {member['gender']}, Is Child: {member['is_child']}")
+
+
+class TheIncredibles(Family):
+    def __init__(self, last_name, members):
+        super().__init__(last_name, members)
+
+    def use_power(self, name):
+        for member in self.members:
+            if member['name'] == name:
+                if member['age'] >= 18:
+                    print(f"{member['name']}'s power is: {member['power']}")
+                else:
+                    raise Exception(f"{member['name']} is not over 18 years old and cannot use their power!")
+                return
+        print(f"No member named {name} found in the family.")
+
+    def incredible_presentation(self):
+        print("Here is our powerful family **")
+        super().family_presentation()
+        for member in self.members:
+            print(f"Incredible Name: {member['incredible_name']}, Power: {member['power']}")
+
+
+incredible_members = [
+    {'name': 'Michael', 'age': 35, 'gender': 'Male', 'is_child': False, 'power': 'fly', 'incredible_name': 'MikeFly'},
+    {'name': 'Sarah', 'age': 32, 'gender': 'Female', 'is_child': False, 'power': 'read minds', 'incredible_name': 'SuperWoman'}
+]
+
+incredibles_family = TheIncredibles("Incredibles", incredible_members)
+
+
+incredibles_family.incredible_presentation()
+
+incredibles_family.born(name='Jack', age=0, gender='Male', is_child=True, power='Unknown Power', incredible_name='BabyJack')
+
+
+incredibles_family.incredible_presentation()
+
+
+try:
+    incredibles_family.use_power('Michael')  
+    incredibles_family.use_power('Jack')     
+except Exception as e:
+    print(e)
